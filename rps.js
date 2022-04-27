57cfdcac6e738c34b7cad76b7f5bac3bf5859d49
ars1an <arslanmansoor22@gmail.com>
2022-04-27 20:13:22 +0530

    Added playRound() and playGame() functionsto finish basic console rps

--- rps.js
+++ rps.js
@@ -3,6 +3,38 @@ function comPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-let initUserChoice = prompt("Rock, Paper Or Scissors?").toLowerCase();
-const userChoice = initUserChoice.charAt(0).toUpperCase() + initUserChoice.slice(1);
-const compChoice = comPlay();
+function playRound(userSelection, compSelection) {
+    if (userSelection === compSelection) {
+        return("It's A Draw!");
+    } else if (userSelection === "Rock" && compSelection === "Paper" || userSelection === "Paper" && compSelection === "Scissor" || userSelection === "Paper" && compSelection === "Scissor") {
+        return(`You Lose! ${compSelection} beats ${userSelection}`);
+    } else if (userSelection === "Scissor" && compSelection === "Paper" || userSelection === "Paper" && compSelection === "Rock" || userSelection === "Rock" && compSelection === "Scissor") {
+        return(`You Won! ${userSelection} beats ${compSelection}`);
+    }
+}
+
+function playGame(n) {
+    let userWin = 0;
+    let compWin = 0;
+    for (i = 0; i < n; i++) {
+        let initUserChoice = prompt("Rock, Paper Or Scissor?").toLowerCase();
+        const userChoice = initUserChoice.charAt(0).toUpperCase() + initUserChoice.slice(1);
+        const compChoice = comPlay();
+        let winner = playRound(userChoice, compChoice);
+        console.log(winner);
+        if (winner.slice(4, 8) === 'Lose') {
+            compWin = compWin + 1;
+        } else if (winner.slice(4, 7) === 'Won') {
+            userWin = userWin + 1;
+        }
+    }
+    if (userWin > compWin) {
+        console.log("YOU ARE THE WINNER");
+    } else if (userWin < compWin) {
+        console.log("YOU LOSE TO THE TECH OVERLORDS");
+    } else {
+        console.log("NOBODY WINS SADGE");
+    }
+} 
+
+playGame(5);
