2b48926154f202dd03e0eed395336e115d9e1407
ars1an <arslanmansoor22@gmail.com>
2022-05-04 15:28:52 +0530

    Upgraded RPS with proper ui and dom results

--- rps.js
+++ rps.js
@@ -5,36 +5,70 @@ function comPlay() {
 
 function playRound(userSelection, compSelection) {
     if (userSelection === compSelection) {
-        return("It's A Draw!");
+        document.getElementById('display').textContent = "It's A Draw!";
     } else if (userSelection === "Rock" && compSelection === "Paper" || userSelection === "Paper" && compSelection === "Scissor" || userSelection === "Paper" && compSelection === "Scissor") {
-        return(`You Lose! ${compSelection} beats ${userSelection}`);
+        document.getElementById('display').textContent = `You Lose! ${compSelection} beats ${userSelection}`;
+        compWin++;
     } else if (userSelection === "Scissor" && compSelection === "Paper" || userSelection === "Paper" && compSelection === "Rock" || userSelection === "Rock" && compSelection === "Scissor") {
-        return(`You Won! ${userSelection} beats ${compSelection}`);
+        document.getElementById('display').textContent = `You Won! ${userSelection} beats ${compSelection}`;
+        userWin++;
     }
 }
 
-function playGame(n) {
-    let userWin = 0;
-    let compWin = 0;
-    for (i = 0; i < n; i++) {
-        let initUserChoice = prompt("Rock, Paper Or Scissor?").toLowerCase();
-        const userChoice = initUserChoice.charAt(0).toUpperCase() + initUserChoice.slice(1);
-        const compChoice = comPlay();
-        let winner = playRound(userChoice, compChoice);
-        console.log(winner);
-        if (winner.slice(4, 8) === 'Lose') {
-            compWin = compWin + 1;
-        } else if (winner.slice(4, 7) === 'Won') {
-            userWin = userWin + 1;
-        }
-    }
-    if (userWin > compWin) {
-        console.log("YOU ARE THE WINNER");
-    } else if (userWin < compWin) {
-        console.log("YOU LOSE TO THE TECH OVERLORDS");
-    } else {
-        console.log("NOBODY WINS SADGE");
-    }
-} 
+function displayScore() {
+    document.getElementById('user').textContent = userWin;
+    document.getElementById('comp').textContent = compWin;     
+}
 
-playGame(5);
+function resetGlobalVariables() {
+    userWin = 0;
+    compWin = 0;
+}
+
+function gameOver () {
+    const buttons = document.querySelectorAll('.buttons');
+    buttons.forEach((button) => {
+        button.removeEventListener('click', playButton);
+    });
+}
+
+function playButton(e) {
+    const compChoice = comPlay();
+    console.log(e.target.id);
+    if (e.target.id === 'rock') {
+        playRound('Rock', compChoice);
+        displayScore();
+    } else if (e.target.id === 'paper') {
+        playRound('Paper', compChoice);
+        displayScore();
+    } else if (e.target.id === 'scissor') {
+        playRound('Scissor', compChoice);
+        displayScore();
+    }
+    if (userWin == 5) {
+        document.getElementById('display').textContent = "YOU HAVE DEFEATED THE COMPUTER OVERLORDS!";
+        gameOver();
+        return;
+    } else if (compWin == 5) {
+        document.getElementById('display').textContent = "YOU LOST! THIS WORLD IS FINISHED";
+        gameOver();
+        return;
+    }
+}
+
+function gameStart() {
+    resetGlobalVariables();
+    displayScore();
+    document.getElementById('display').textContent = "Let's see what you got!";
+    const buttons = document.querySelectorAll('.buttons');
+    buttons.forEach((button) => {
+        button.addEventListener('click', playButton);
+});
+}
+
+let userWin;
+let compWin;
+gameStart();
+const start = document.querySelector('#reset')
+start.addEventListener('click', gameStart);
+
